95a5f7ad7decf2e91d20eab500c9dba202f5c1a3
Sven Willner <sven.willner@gmail.com>
2019-07-16 15:35:53 +0200

    Fix compiler tests

--- netcdftools.h
+++ netcdftools.h
@@ -18,18 +18,18 @@
 #ifndef NETCDFTOOLS_H
 #define NETCDFTOOLS_H
 
-#if __CUDACC__
+#if defined(__CUDACC__)
 #pragma push
 #pragma diag_suppress = useless_type_qualifier_on_return_type
-#elif __GNUC__
+#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#pragma warning push
+#pragma warning disable : 858
+#elif defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #pragma GCC diagnostic ignored "-Wunused-variable"
-#elif _MSC_VER || __INTEL_COMPILER
-#pragma warning push
-#pragma warning disable : 858
 #endif
 
 #include <ncDim.h>
@@ -39,12 +39,12 @@
 #include <ncVar.h>
 #include <netcdf>
 
-#if __CUDACC__
+#if defined(__CUDACC__)
 #pragma pop
-#elif __GNUC__
-#pragma GCC diagnostic pop
-#elif _MSC_VER || __INTEL_COMPILER
+#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
 #pragma warning pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
 #endif
 
 #include <algorithm>
