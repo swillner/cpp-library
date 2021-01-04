f7a8b586d1ab5f592cb092d379b20d5cc87d6002
Sven Willner <sven.willner@gmail.com>
2021-01-04 14:24:47 +0100

    Overhaul autodiff

--- autodiff.h
+++ autodiff.h
@@ -35,178 +35,182 @@ class Value;
 namespace std {
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const autodiff::Value<T, Vector>& rhs);
+inline autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const autodiff::Value<T, Vector>& rhs);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const T& val, const autodiff::Value<T, Vector>& rhs);
+inline autodiff::Value<T, Vector> pow(const T& val, const autodiff::Value<T, Vector>& rhs);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const T& val);
+inline autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const T& val);
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log(const autodiff::Value<T, Vector>& v);
+inline autodiff::Value<T, Vector> log(const autodiff::Value<T, Vector>& v);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log2(const autodiff::Value<T, Vector>& v);
+inline autodiff::Value<T, Vector> log2(const autodiff::Value<T, Vector>& v);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log10(const autodiff::Value<T, Vector>& v);
+inline autodiff::Value<T, Vector> log10(const autodiff::Value<T, Vector>& v);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> exp(const autodiff::Value<T, Vector>& v);
+inline autodiff::Value<T, Vector> exp(const autodiff::Value<T, Vector>& v);
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> min(const T& val, const autodiff::Value<T, Vector>& rhs);
+inline autodiff::Value<T, Vector> min(const T& val, const autodiff::Value<T, Vector>& rhs);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> min(const autodiff::Value<T, Vector>& lhs, const T& val);
+inline autodiff::Value<T, Vector> min(const autodiff::Value<T, Vector>& lhs, const T& val);
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> max(const T& val, const autodiff::Value<T, Vector>& rhs);
+inline autodiff::Value<T, Vector> max(const T& val, const autodiff::Value<T, Vector>& rhs);
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> max(const autodiff::Value<T, Vector>& lhs, const T& val);
+inline autodiff::Value<T, Vector> max(const autodiff::Value<T, Vector>& lhs, const T& val);
 
 }  // namespace std
 
 namespace autodiff {
 
 template<typename T, typename Vector>
-class Value {
+class Value final {
     friend class Variable<T, Vector>;
 
-  protected:
+  private:
     T val;
     Vector dev;
-    constexpr Value(const T& v, const Vector& dev_p) : val(v), dev(dev_p) {}
+    Value(T v, Vector dev_p) : val(std::move(v)), dev(std::move(dev_p)) {}
 
   public:
-    constexpr Value(std::size_t n, const T& v) : val(v), dev(0.0, n) {}
-    constexpr Value(std::size_t i, std::size_t n, const T& v) : val(v), dev(0.0, n) { dev[i] = 1; }
-    explicit constexpr operator T() const { return val; }
-    constexpr const T value() const { return val; }
-    constexpr const Vector& derivative() const { return dev; }
+    Value(std::size_t n, T v) : val(std::move(v)), dev(0.0, n) {}
+    Value(std::size_t i, std::size_t n, T v) : val(std::move(v)), dev(0.0, n) { dev[i] = 1; }
 
-    constexpr Value operator-() { return {-val, -dev}; }
+    std::size_t size() const { return dev.size(); }
+    const T& value() const { return val; }
+    const Vector& derivative() const { return dev; }
+    explicit operator T() const { return val; }
 
-    inline friend Value operator+(const Value& lhs, const Value& rhs) { return {lhs.val + rhs.val, lhs.dev + rhs.dev}; }
-    inline friend Value operator+(const T& v, const Value& rhs) { return {v + rhs.val, rhs.dev}; }
-    inline friend Value operator+(const Value& lhs, const T& v) { return {lhs.val + v, lhs.dev}; }
+    Value operator-() const { return {-val, -dev}; }
 
-    inline friend Value operator-(const Value& lhs, const Value& rhs) { return {lhs.val - rhs.val, lhs.dev - rhs.dev}; }
-    inline friend Value operator-(const T& v, const Value& rhs) { return {v - rhs.val, -rhs.dev}; }
-    inline friend Value operator-(const Value& lhs, const T& v) { return {lhs.val - v, lhs.dev}; }
+    friend Value operator+(const Value& lhs, const Value& rhs) { return {lhs.val + rhs.val, lhs.dev + rhs.dev}; }
+    friend Value operator+(const T& v, const Value& rhs) { return {v + rhs.val, rhs.dev}; }
+    friend Value operator+(const Value& lhs, const T& v) { return {lhs.val + v, lhs.dev}; }
 
-    inline friend Value operator*(const Value& lhs, const Value& rhs) { return {lhs.val * rhs.val, lhs.dev * rhs.val + lhs.val * rhs.dev}; }
-    inline friend Value operator*(const T& v, const Value& rhs) { return {v * rhs.val, v * rhs.dev}; }
-    inline friend Value operator*(const Value& lhs, const T& val) { return {lhs.val * val, lhs.dev * val}; }
+    friend Value operator-(const Value& lhs, const Value& rhs) { return {lhs.val - rhs.val, lhs.dev - rhs.dev}; }
+    friend Value operator-(const T& v, const Value& rhs) { return {v - rhs.val, -rhs.dev}; }
+    friend Value operator-(const Value& lhs, const T& v) { return {lhs.val - v, lhs.dev}; }
 
-    inline friend Value operator/(const Value& lhs, const Value& rhs) {
-        return {lhs.val / rhs.val, lhs.dev / rhs.val - rhs.dev * (lhs.val / rhs.val / rhs.val)};
-    }
-    inline friend Value operator/(const T& v, const Value& rhs) { return {v / rhs.val, rhs.dev * (-v / rhs.val / rhs.val)}; }
-    inline friend Value operator/(const Value& lhs, const T& v) { return {lhs.val / v, lhs.dev / v}; }
+    friend Value operator*(const Value& lhs, const Value& rhs) { return {lhs.val * rhs.val, lhs.dev * rhs.val + lhs.val * rhs.dev}; }
+    friend Value operator*(const T& v, const Value& rhs) { return {v * rhs.val, v * rhs.dev}; }
+    friend Value operator*(const Value& lhs, const T& val) { return {lhs.val * val, lhs.dev * val}; }
 
-    constexpr Value& operator+=(const Value& v) {
+    friend Value operator/(const Value& lhs, const Value& rhs) { return {lhs.val / rhs.val, lhs.dev / rhs.val - rhs.dev * (lhs.val / rhs.val / rhs.val)}; }
+    friend Value operator/(const T& v, const Value& rhs) { return {v / rhs.val, rhs.dev * (-v / rhs.val / rhs.val)}; }
+    friend Value operator/(const Value& lhs, const T& v) { return {lhs.val / v, lhs.dev / v}; }
+
+    Value& operator+=(const Value& v) {
         val += v.val;
         dev += v.dev;
         return *this;
     }
-    constexpr Value& operator+=(const T& v) {
+    Value& operator+=(const T& v) {
         val += v;
         return *this;
     }
 
-    constexpr Value& operator-=(const Value& v) {
+    Value& operator-=(const Value& v) {
         val -= v.val;
         dev -= v.dev;
         return *this;
     }
-    constexpr Value& operator-=(const T& v) {
+    Value& operator-=(const T& v) {
         val -= v;
         return *this;
     }
 
-    constexpr Value& operator*=(const Value& v) {
+    Value& operator*=(const Value& v) {
         val *= v.val;
         dev = dev * v.val + v.dev * val;
         return *this;
     }
-    constexpr Value& operator*=(const T& v) {
+    Value& operator*=(const T& v) {
         val *= v;
         dev *= v;
         return *this;
     }
 
-    constexpr Value& operator/=(const Value& v) {
+    Value& operator/=(const Value& v) {
         val /= v.val;
         dev = dev / v.val - v.dev * val / v.val / v.val;
         return *this;
     }
-    constexpr Value& operator/=(const T& v) {
+    Value& operator/=(const T& v) {
         val /= v;
         dev /= v;
         return *this;
     }
 
-    constexpr friend bool operator<(const Value& lhs, const Value& rhs) { return lhs.val < rhs.val; }
-    constexpr friend bool operator<(const T& v, const Value& rhs) { return v < rhs.val; }
-    constexpr friend bool operator<(const Value& lhs, const T& v) { return lhs.val < v; }
+    friend bool operator<(const Value& lhs, const Value& rhs) { return lhs.val < rhs.val; }
+    friend bool operator<(const T& v, const Value& rhs) { return v < rhs.val; }
+    friend bool operator<(const Value& lhs, const T& v) { return lhs.val < v; }
 
-    constexpr friend bool operator<=(const Value& lhs, const Value& rhs) { return lhs.val <= rhs.val; }
-    constexpr friend bool operator<=(const T& v, const Value& rhs) { return v <= rhs.val; }
-    constexpr friend bool operator<=(const Value& lhs, const T& v) { return lhs.val <= v; }
+    friend bool operator<=(const Value& lhs, const Value& rhs) { return lhs.val <= rhs.val; }
+    friend bool operator<=(const T& v, const Value& rhs) { return v <= rhs.val; }
+    friend bool operator<=(const Value& lhs, const T& v) { return lhs.val <= v; }
 
-    constexpr friend bool operator>(const Value& lhs, const Value& rhs) { return lhs.val > rhs.val; }
-    constexpr friend bool operator>(const T& v, const Value& rhs) { return v > rhs.val; }
-    constexpr friend bool operator>(const Value& lhs, const T& v) { return lhs.val > v; }
+    friend bool operator>(const Value& lhs, const Value& rhs) { return lhs.val > rhs.val; }
+    friend bool operator>(const T& v, const Value& rhs) { return v > rhs.val; }
+    friend bool operator>(const Value& lhs, const T& v) { return lhs.val > v; }
 
-    constexpr friend bool operator>=(const Value& lhs, const Value& rhs) { return lhs.val >= rhs.val; }
-    constexpr friend bool operator>=(const T& v, const Value& rhs) { return v >= rhs.val; }
-    constexpr friend bool operator>=(const Value& lhs, const T& v) { return lhs.val >= v; }
+    friend bool operator>=(const Value& lhs, const Value& rhs) { return lhs.val >= rhs.val; }
+    friend bool operator>=(const T& v, const Value& rhs) { return v >= rhs.val; }
+    friend bool operator>=(const Value& lhs, const T& v) { return lhs.val >= v; }
 
-    constexpr friend bool operator==(const Value& lhs, const Value& rhs) { return lhs.val == rhs.val; }
-    constexpr friend bool operator==(const T& v, const Value& rhs) { return v == rhs.val; }
-    constexpr friend bool operator==(const Value& lhs, const T& v) { return lhs.val == v; }
+    friend bool operator==(const Value& lhs, const Value& rhs) { return lhs.val == rhs.val; }
+    friend bool operator==(const T& v, const Value& rhs) { return v == rhs.val; }
+    friend bool operator==(const Value& lhs, const T& v) { return lhs.val == v; }
 
-    constexpr friend bool operator!=(const Value& lhs, const Value& rhs) { return lhs.val != rhs.val; }
-    constexpr friend bool operator!=(const T& v, const Value& rhs) { return v != rhs.val; }
-    constexpr friend bool operator!=(const Value& lhs, const T& v) { return lhs.val != v; }
+    friend bool operator!=(const Value& lhs, const Value& rhs) { return lhs.val != rhs.val; }
+    friend bool operator!=(const T& v, const Value& rhs) { return v != rhs.val; }
+    friend bool operator!=(const Value& lhs, const T& v) { return lhs.val != v; }
 
-    constexpr friend Value std::pow<T, Vector>(const Value& lhs, const Value& rhs);
-    constexpr friend Value std::pow<T, Vector>(const T& v, const Value& rhs);
-    constexpr friend Value std::pow<T, Vector>(const Value& lhs, const T& v);
+    friend Value std::pow<T, Vector>(const Value& lhs, const Value& rhs);
+    friend Value std::pow<T, Vector>(const T& v, const Value& rhs);
+    friend Value std::pow<T, Vector>(const Value& lhs, const T& v);
 
-    constexpr friend Value std::log<T, Vector>(const Value& v);
-    constexpr friend Value std::log2<T, Vector>(const Value& v);
-    constexpr friend Value std::log10<T, Vector>(const Value& v);
-    constexpr friend Value std::exp<T, Vector>(const Value& v);
+    friend Value std::log<T, Vector>(const Value& v);
+    friend Value std::log2<T, Vector>(const Value& v);
+    friend Value std::log10<T, Vector>(const Value& v);
+    friend Value std::exp<T, Vector>(const Value& v);
 
-    constexpr friend Value std::min<T, Vector>(const T& v, const Value& rhs);
-    constexpr friend Value std::min<T, Vector>(const Value& lhs, const T& v);
+    friend Value std::min<T, Vector>(const T& v, const Value& rhs);
+    friend Value std::min<T, Vector>(const Value& lhs, const T& v);
 
-    constexpr friend Value std::max<T, Vector>(const T& v, const Value& rhs);
-    constexpr friend Value std::max<T, Vector>(const Value& lhs, const T& v);
+    friend Value std::max<T, Vector>(const T& v, const Value& rhs);
+    friend Value std::max<T, Vector>(const Value& lhs, const T& v);
 };
 
 template<typename T, typename Vector>
-class Variable {
-  protected:
+class Variable final {
+  private:
     std::vector<T> val;
     const std::size_t variables_num;
     const std::size_t variables_offset;
 
   public:
-    constexpr Variable(std::size_t offset, std::size_t num, std::size_t length, const T& initial_value)
-        : variables_num(num), variables_offset(offset), val(length, initial_value) {}
-    constexpr const Variable& operator=(const std::vector<T>& v) {
+    Variable(std::size_t offset, std::size_t num, std::size_t length, T initial_value)
+        : variables_num(num), variables_offset(offset), val(length, std::move(initial_value)) {}
+
+    Variable& operator=(const std::vector<T>& v) {
         val.assign(v);
         return *this;
     }
-    constexpr std::size_t size() const { return val.size(); }
-    constexpr std::vector<T>& value() { return val; }
-    constexpr const std::vector<T>& value() const { return val; }
-    constexpr Value<T, Vector> operator[](std::size_t i) const {
+
+    std::size_t size() const { return val.size(); }
+
+    std::vector<T>& value() { return val; }
+    const std::vector<T>& value() const { return val; }
+
+    Value<T, Vector> operator[](std::size_t i) const {
         if (variables_offset < variables_num) {
             return {i + variables_offset, variables_num, val[i]};
         } else {
             return {variables_num, val[i]};
         }
     }
-    constexpr Value<T, Vector> at(std::size_t i) const {
+    Value<T, Vector> at(std::size_t i) const {
         if (variables_offset < variables_num) {
             return {i + variables_offset, variables_num, val.at(i)};
         } else {
@@ -220,45 +224,45 @@ class Variable {
 namespace std {
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const autodiff::Value<T, Vector>& rhs) {
+inline autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const autodiff::Value<T, Vector>& rhs) {
     const T p = pow(lhs.val, rhs.val);
     return {p, rhs.dev * (log(lhs.val) * p) + lhs.dev * (p * rhs.val / lhs.val)};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const T& v, const autodiff::Value<T, Vector>& rhs) {
+inline autodiff::Value<T, Vector> pow(const T& v, const autodiff::Value<T, Vector>& rhs) {
     const T p = pow(v, rhs.val);
     return {p, rhs.dev * (p * log(v))};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const T& v) {
+inline autodiff::Value<T, Vector> pow(const autodiff::Value<T, Vector>& lhs, const T& v) {
     return {pow(lhs.val, v), lhs.dev * (v * pow(lhs.val, v - 1))};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log(const autodiff::Value<T, Vector>& v) {
+inline autodiff::Value<T, Vector> log(const autodiff::Value<T, Vector>& v) {
     return {log(v.val), v.dev / v.val};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log2(const autodiff::Value<T, Vector>& v) {
+inline autodiff::Value<T, Vector> log2(const autodiff::Value<T, Vector>& v) {
     return {log2(v.val), v.dev / (v.val * M_LN2)};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> log10(const autodiff::Value<T, Vector>& v) {
+inline autodiff::Value<T, Vector> log10(const autodiff::Value<T, Vector>& v) {
     return {log10(v.val), v.dev / (v.val * M_LN10)};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> exp(const autodiff::Value<T, Vector>& v) {
+inline autodiff::Value<T, Vector> exp(const autodiff::Value<T, Vector>& v) {
     const T e = exp(v.val);
     return {e, v.dev * e};
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> min(const T& v, const autodiff::Value<T, Vector>& rhs) {
+inline autodiff::Value<T, Vector> min(const T& v, const autodiff::Value<T, Vector>& rhs) {
     if (v < rhs.val) {
         return {rhs.dev.size(), v};
     } else {
@@ -267,7 +271,7 @@ constexpr autodiff::Value<T, Vector> min(const T& v, const autodiff::Value<T, Ve
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> min(const autodiff::Value<T, Vector>& lhs, const T& v) {
+inline autodiff::Value<T, Vector> min(const autodiff::Value<T, Vector>& lhs, const T& v) {
     if (lhs.val < v) {
         return lhs;
     } else {
@@ -276,7 +280,7 @@ constexpr autodiff::Value<T, Vector> min(const autodiff::Value<T, Vector>& lhs, 
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> max(const T& v, const autodiff::Value<T, Vector>& rhs) {
+inline autodiff::Value<T, Vector> max(const T& v, const autodiff::Value<T, Vector>& rhs) {
     if (v < rhs.val) {
         return rhs;
     } else {
@@ -285,7 +289,7 @@ constexpr autodiff::Value<T, Vector> max(const T& v, const autodiff::Value<T, Ve
 }
 
 template<typename T, typename Vector>
-constexpr autodiff::Value<T, Vector> max(const autodiff::Value<T, Vector>& lhs, const T& v) {
+inline autodiff::Value<T, Vector> max(const autodiff::Value<T, Vector>& lhs, const T& v) {
     if (lhs.val < v) {
         return {lhs.dev.size(), v};
     } else {
